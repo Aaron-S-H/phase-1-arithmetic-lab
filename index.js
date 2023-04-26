0ab8762617dc06ae99364582461963a1bb527b9e
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-04-26 20:53:41 +0200

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.ceil(Math.random());
+
+const num3 = 25;
+const num4 = 7;
+let mod = num3 % num4;
+
+let max = Math.max(1, 4, 6, 9, 20);
